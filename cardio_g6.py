9b41a43704415d826cb57b92d676630763bf90aa
Felix <santosgonzales1203@gmail.com>
2024-11-21 14:19:46 -0500

    oo

--- cardio_g6.py
+++ cardio_g6.py
@@ -12,9 +12,15 @@ st.write("Ingrese los datos del paciente para predecir la probabilidad de proble
 
 # Cargar el dataset
 #dataset_path = '/mnt/data/ddf_cardio_data_fin_EDA.pickle'
-dataset_path = 'ddf_cardio_data_fin_EDA.pickle'
+
+#
+#dataset_path = 'ddf_cardio_data_fin_EDA.pickle' 
+dataset_path = 'modelo_ganador_rf.pkl'
+
 #dataset_path = 'modelo_ganador_logreg.pkl'
-df_data = pd.read_pickle(dataset_path)
+
+#df_data = pd.read_pickle(dataset_path)
+dataset_path = joblib.load(dataset_path)
 
 # Preprocesamiento del dataset
 df_cat = df_data.select_dtypes(exclude='number').copy()
